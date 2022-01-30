de49e8d70dc8f1b5d4b545a2996a000d49652bd1
kyriakatos1971 <kyriakos.kyriakatos@outlook.com>
2022-01-30 15:06:12 +0100

    all

---
+++ calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+/Users/kyriakoskyriakatos/github/kyriakatos1971/calculator/target/debug/calculator: /Users/kyriakoskyriakatos/github/kyriakatos1971/calculator/src/main.rs
---
+++ calculator/target/debug/deps/calculator-f31ccdf8d711c8cf.d
@@ -0,0 +1,5 @@
+/Users/kyriakoskyriakatos/github/kyriakatos1971/calculator/target/debug/deps/calculator-f31ccdf8d711c8cf: src/main.rs
+
+/Users/kyriakoskyriakatos/github/kyriakatos1971/calculator/target/debug/deps/calculator-f31ccdf8d711c8cf.d: src/main.rs
+
+src/main.rs:
